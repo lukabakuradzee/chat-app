95dd4344b3e2833a5e67e4790ba166f9d6bc450c
lukabakuradzee <lukabakuradzee@gmail.com>
2024-03-04 17:57:14 +0400

    chat featuers, icons, emoji picker

--- chat-app/src/pages/Home/Chat.jsx
+++ chat-app/src/pages/Home/Chat.jsx
@@ -1,43 +1,107 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useAuthContext } from '../../context/auth/AuthContextProvider';
+import EmojiPicker from 'emoji-picker-react';
 
-const Chat = () => {
-  const [message, setMessage] = useState('');
+const Chat = ({ chatMessages }) => {
+  const chatContainerRef = useRef(null);
+  const [inputMessage, setInputMessage] = useState('');
   const [messages, setMessages] = useState([]);
   const { state } = useAuthContext();
-  const { user } = state;
+  const { user: currentUser } = state;
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
 
-  const handleChange = (event) => {
-    setMessage(event.target.value);
+  const handleMessageChange = (event) => {
+    setInputMessage(event.target.value);
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    if (message.trim() === '') {
+    if (inputMessage.trim() === '') {
       return; // Prevent sending empty messages
     }
     // Add the message to the list of messages
-    setMessages([...messages, { text: message, sender: 'user' }]);
+    setMessages([
+      ...messages,
+      { text: inputMessage, sender: currentUser.userName },
+    ]);
     // Clear the input field
-    setMessage('');
+    setInputMessage('');
+  };
+
+  const handleFormSubmit = (event) => {
+    if (event) {
+      event.preventDefault();
+      handleSubmit(event);
+    }
+  };
+
+  useEffect(() => {
+    // Scroll chat container to the bottom whenever messages change
+    if (chatContainerRef.current) {
+      chatContainerRef.current.scrollTop =
+        chatContainerRef.current.scrollHeight;
+    }
+  }, [chatMessages]);
+
+  const toggleEmojiPicker = () => {
+    setShowEmojiPicker((prevState) => !prevState);
+  };
+
+const handleKeyDown = (event) => { 
+    if(event.key === 'Escape') {
+      setShowEmojiPicker(false);
+    }
+ }
+
+ useEffect(() => {
+  document.addEventListener('keydown', handleKeyDown)
+   return () => {
+     document.removeEventListener('keydown', handleKeyDown)
+   }
+ }, [])
+ 
+
+ 
+
+  const handleEmojiClick = (emoji) => {
+    setInputMessage(inputMessage + emoji);
   };
 
   return (
     <div className="chat-container">
-      <div className="chat-messages">
+      <div className="emoji-picker-container">
+        {showEmojiPicker && (
+          <EmojiPicker
+            onEmojiClick={(emojiObject) => handleEmojiClick(emojiObject.emoji)}
+            disableAutoFocus={true}
+            style={{ height:"400px", backgroundColor: '#fe3c72', borderStyle: 'none' }}
+          />
+        )}
+      </div>
+      <div ref={chatContainerRef} className="chat-messages">
         {messages.map((msg, index) => (
-          <div key={index} className={`message ${msg.sender}`}>
-           <span className="sender">{user.userName}</span>: <span className="message-text-chat">{msg.text}</span>
+          <div
+            key={index}
+            className={`message ${msg.sender === currentUser.userName ? 'current-user' : 'other-user'}`}
+          >
+            <span className="sender">{currentUser.userName}</span>:{' '}
+            <span className="message-text-chat">{msg.text}</span>
           </div>
         ))}
       </div>
-      <form onSubmit={handleSubmit} className="chat-input">
+      <form onSubmit={handleFormSubmit} className="chat-input">
         <input
           type="text"
           placeholder="Type your message..."
-          value={message}
-          onChange={handleChange}
+          value={inputMessage}
+          onChange={handleMessageChange}
         />
+
+        <i
+          class="fa-solid fa-face-smile emoji-icon-click"
+          onClick={toggleEmojiPicker}
+        ></i>
+
         <button type="submit" className="send-message-button">
           Send
         </button>
