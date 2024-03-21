721010ee2318a34ee7719d09d9cbec696681e7c6
lukabakuradzee <lukabakuradzee@gmail.com>
2024-03-21 18:04:06 +0400

    slider

--- chat-app/src/components/UserProfile/UserPageContainer.jsx
+++ chat-app/src/components/UserProfile/UserPageContainer.jsx
@@ -6,7 +6,7 @@ const UserPageContainer = () => {
   const user = {
     username: 'luka92',
     profilePicture:
-      "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?cs=srgb&dl=pexels-stefan-stefancik-91227.jpg&fm=jpg",
+      'https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?cs=srgb&dl=pexels-stefan-stefancik-91227.jpg&fm=jpg',
     userPosts: 6,
     followersCount: 100,
     followingCount: 50,
@@ -16,44 +16,55 @@ const UserPageContainer = () => {
   const posts = [
     {
       id: 1,
-      imageUrl:
+      imageUrl: [
         'https://static.photocrowd.com/upl/kD/cms.CZEIHIQ_-STGXv3K5VXw-hd.jpeg',
+        'https://images.squarespace-cdn.com/content/v1/576b017229687f1fd26b9117/1598188832992-9VFDGBTDCT86PWBA1J0X/001_35mmStreet-Masks.jpg',
+      ],
       caption: 'Beautiful sunset!',
     },
     {
       id: 2,
-      imageUrl:
+      imageUrl: [
         'https://www.thewholeworldisaplayground.com/wp-content/uploads/2019/01/Italy-Florence-Best-Photo-Locations-2.webp',
+        'https://amateurphotographer.com/wp-content/uploads/sites/7/2023/03/jessica-knowlden-nyBUfH9MkL4-unsplash.jpg?w=900',
+      ],
       caption: 'Exploring nature!',
     },
     {
       id: 3,
-      imageUrl:
+      imageUrl: [
         'https://cdmtridentonline.com/wp-content/uploads/2020/02/kiana--900x506.jpg',
+        'https://streetphotographersfoundation.com/wp-content/uploads/2020/07/steve.jpg',
+      ],
       caption: 'Having fun at the beach!',
     },
     {
       id: 4,
-      imageUrl:
+      imageUrl: [
         'https://iso.500px.com/wp-content/uploads/2015/09/streetcover1.jpeg',
+        'https://shotkit.com/wp-content/uploads/2023/02/famous-street-photographer.jpg',
+      ],
       caption: 'Having fun at the beach!',
     },
     {
       id: 5,
-      imageUrl:
+      imageUrl: [
         'https://images.unsplash.com/photo-1610247948543-28d438fd094b?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8c3RyZWV0JTIwcGhvdG9ncmFwaHl8ZW58MHx8MHx8fDA%3D',
+        'https://cdn.fstoppers.com/styles/full/s3/media/2023/05/04/nando-street-photography-7.jpg',
+      ],
       caption: 'Having fun at the beach!',
     },
     {
       id: 6,
-      imageUrl:
+      imageUrl: [
         'https://www.photographynews.co.uk/wp-content/uploads/tom-parsons-ow_3Zc8xSN4-unsplash.jpg',
+        'https://www.artribune.com/wp-content/uploads/2020/10/Eolo-Perfido-Street-Photography-Milano.jpg',
+      ],
       caption: 'Having fun at the beach!',
     },
   ];
 
   return <UserProfilePage {...user} posts={posts} />;
 };
-
 
 export default UserPageContainer;
--- chat-app/src/components/UserProfile/UserProfilePage.jsx
+++ chat-app/src/components/UserProfile/UserProfilePage.jsx
@@ -1,6 +1,20 @@
 import { useState } from 'react';
 import useEscapeKeyHandler from '../../Hooks/EscapeHandler';
 import UserComment from './UserComment';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import React from 'react';
+
+const sliderSettings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: false,
+  autoplaySpeed: 2000,
+};
 
 const UserProfilePage = ({
   username,
@@ -12,10 +26,11 @@ const UserProfilePage = ({
 }) => {
   const [selectedPost, setSelectedPost] = useState(null);
 
-
   const handleClickPost = (postId) => {
-    setSelectedPost(postId);
+    setSelectedPost(postId === selectedPost ? null : postId);
   };
+  console.log('selectedPost:', selectedPost);
+  console.log('posts:', posts);
 
   useEscapeKeyHandler(() => setSelectedPost(false));
 
@@ -25,7 +40,7 @@ const UserProfilePage = ({
         <div className="user-photo-header">
           <img src={profilePicture} alt="Profile" />
           <h1>{username}</h1>
-          <div className='btn-container-user-profile'>
+          <div className="btn-container-user-profile">
             <button className="edit-profile-user-btn">Edit Profile</button>
             <button className="view-archive-profile-user">View archive</button>
           </div>
@@ -47,11 +62,27 @@ const UserProfilePage = ({
               className="post"
               onClick={() => handleClickPost(post.id)}
             >
-              <img src={post.imageUrl} alt="Post" />
+              <Slider {...sliderSettings}>
+                {Array.isArray(post.imageUrl) ? (
+                  post.imageUrl.map((image, index) => (
+                    <div key={index}>
+                      <img src={image} alt={`Post ${index + 1}`} />
+                    </div>
+                  ))
+                ) : (
+                  <div>
+                    <img src={post.imageUrl} alt={`Post ${post.id}`} />
+                  </div>
+                )}
+              </Slider>
               {selectedPost === post.id && (
                 <UserComment
                   key={post.id}
-                  imageUrl={post.imageUrl}
+                  imageUrl={
+                    Array.isArray(post.imageUrl)
+                      ? post.imageUrl[0]
+                      : post.imageUrl
+                  }
                   profilePicutre={profilePicture}
                   username={username}
                 />
@@ -59,6 +90,7 @@ const UserProfilePage = ({
             </div>
           ))}
       </div>
+
       {selectedPost && (
         <div className="page-overlay" onClick={() => setSelectedPost(null)}>
           {/* Additional content for the page overlay */}
