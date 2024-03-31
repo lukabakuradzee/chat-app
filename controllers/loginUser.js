3c22c344b9c923eb20698b512c7b8e23f3b41893
lukabakuradzee <lukabakuradzee@gmail.com>
2024-04-01 02:21:33 +0400

    change

--- controllers/loginUser.js
+++ controllers/loginUser.js
@@ -47,7 +47,7 @@ exports.loginUser = async (req, res) => {
         subject: 'Password Reset Instructions',
         html: `<p>You are receiving this email because you (or someone else) has requested the reset of the password for your account.</p>
               <p>Please click on the following link, or paste this into your browser to complete the process:</p>
-              <p>http://localhost:3000/reset-password?token=${resetToken}</p>
+              <p>http://localhost:5500/reset-password?token=${resetToken}</p>
               <p>If you did not request this, please ignore this email and your password will remain unchanged.</p>`
       };
 
