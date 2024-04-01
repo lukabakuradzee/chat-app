5b92eb2c9a86396cfd6c908c8301bd25f9cf2c39
lukabakuradzee <lukabakuradzee@gmail.com>
2024-04-01 10:01:25 +0400

    Condition of username already exists

--- controllers/registerUser.js
+++ controllers/registerUser.js
@@ -17,24 +17,29 @@ const sendVerificationEmail = async (email, token) => {
     service: "gmail",
     auth: {
       user: "lukabakuradze39@gmail.com",
-      pass: "nomzlhdqzjxxhlms"
+      pass: "nomzlhdqzjxxhlms",
     },
   });
-  
+
   // Compose email message
   const mailOptions = {
-    from: 'lukabakuradze39@gmail.com',
+    from: "lukabakuradze39@gmail.com",
     to: email,
-    subject: 'Email Verification',
-    text: `Please click on the following link to verify your email: http://yourdomain.com/verify-email?token=${token}`
-};
+    subject: "Email Verification",
+    text: `Please click on the following link to verify your email: http://yourdomain.com/verify-email?token=${token}`,
+  };
 
-  await transporter.sendMail(mailOptions)
+  await transporter.sendMail(mailOptions);
 };
 
 exports.registerUser = async (req, res) => {
   try {
     const { username, name, lastName, age, email, password } = req.body;
+    // Check if username exists
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ message: "Username already exists" });
+    }
 
     // generate token
     const verificationToken = generateVerificationToken(email);
@@ -53,12 +58,12 @@ exports.registerUser = async (req, res) => {
       age,
       email,
       password: hashedPassword,
-      verificationToken
+      verificationToken,
     });
     await newUser.save();
 
     // Send Verification email
-    await sendVerificationEmail(email, verificationToken)
+    await sendVerificationEmail(email, verificationToken);
 
     res.status(201).json({ message: "User registered successfully." });
   } catch (error) {
@@ -67,27 +72,26 @@ exports.registerUser = async (req, res) => {
   }
 };
 
-exports.verifyEmail = async (req,res) => {
-    try {
-        const { token } = req.body;
+exports.verifyEmail = async (req, res) => {
+  try {
+    const { token } = req.body;
 
-        // verify token
-        const decoded = jwt.verify(token.split(' ')[1], secretKey);
+    // verify token
+    const decoded = jwt.verify(token.split(" ")[1], secretKey);
 
-        // Find user by email and update email verification status
-        const user = await User.findOneAndUpdate(
-            {email: decoded.email},
-            {$set: {emailVerified: true}},
-            {new: true}
-
-     );
-      if(!user) {
-        return res.status(404).json({message: "User not found"})
-      }
-      
-    res.status(200).json({message: "Email verified successfully"})
-    }  catch (error) {
-        console.error(error);
-        res.status(500).json({message: "Failed to verify Email"})
+    // Find user by email and update email verification status
+    const user = await User.findOneAndUpdate(
+      { email: decoded.email },
+      { $set: { emailVerified: true } },
+      { new: true }
+    );
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
-}
+
+    res.status(200).json({ message: "Email verified successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Failed to verify Email" });
+  }
+};
